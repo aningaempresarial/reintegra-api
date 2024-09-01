0059453674e055b7aee1245e26976ce32ad1d2bc
yotozangue <allandelima@proton.me>
2024-09-01 14:32:24 -0300

    Não exibir usuários removidos =)

--- src/routes/admin-usuario.routes.js
+++ src/routes/admin-usuario.routes.js
@@ -33,7 +33,7 @@ router.route('/')
                     tbEmpresa emp ON u.idUsuario = emp.idUsuario
                 LEFT JOIN
                     tbEscola esc ON u.idUsuario = esc.idUsuario
-                WHERE 1=1
+                WHERE statusEntidade = 'ativo' OR statusEntidade = 'bloqueado'
             `;
 
             if (nome) {
