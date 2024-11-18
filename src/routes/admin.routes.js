15b440b7b526e759deb40ce8348018285ce565c3
Sabrina Cristan <sabrina.cristan@gmail.com>
2024-11-18 20:06:49 -0300

    Listagem de empresas por setor top

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,45 +1,41 @@
 import { Router } from "express";
 import multer from "multer";
-import { query } from '../db/query.js';
+import { query } from "../db/query.js";
 import { getUser } from "../functions/auth/login.js";
 
 const router = Router();
 const upload = multer();
 
-router.route('/getdata')
-    .post(upload.none(), async (req, res) => {
+router.route("/getdata").post(upload.none(), async (req, res) => {
+    const { token } = req.body || {};
 
-        const { token } = req.body || {}
+    if (typeof token == "undefined") {
+        return res.status(400).json({ erro: "`token` não é um campo válido." });
+    }
 
-        if (typeof token == 'undefined') {
-            return res.status(400).json({ erro: "`token` não é um campo válido." });
+    try {
+        const user = await getUser(token);
+
+        if (user[0]) {
+            const usuario = user[1];
+            const consulta = await query(
+                `SELECT emailUsuario, usuario, nomeAdmin, fotoPerfil, bannerPerfil, descricaoPerfil FROM tbAdmin JOIN tbUsuario ON tbAdmin.idUsuario = tbUsuario.idUsuario JOIN tbPerfil ON tbUsuario.idUsuario = tbPerfil.idUsuario WHERE tbUsuario.idUsuario = ${usuario.idUsuario}`
+            );
+            return res.json(consulta[0]);
+        } else {
+            return res.status(404).json({ erro: "Usuario não encontrado." });
         }
+    } catch (erro) {
+        res.status(500).json({
+            erro: "Erro ao processar a solicitação.",
+            detalhe: erro.message,
+        });
+    }
+});
 
-
-        try {
-            const user = await getUser(token);
-
-            if (user[0]) {
-                const usuario = user[1];
-                const consulta = await query(`SELECT emailUsuario, usuario, nomeAdmin, fotoPerfil, bannerPerfil, descricaoPerfil FROM tbAdmin JOIN tbUsuario ON tbAdmin.idUsuario = tbUsuario.idUsuario JOIN tbPerfil ON tbUsuario.idUsuario = tbPerfil.idUsuario WHERE tbUsuario.idUsuario = ${usuario.idUsuario}`);
-                return res.json(consulta[0]);
-            } else {
-                return res.status(404).json({ erro: 'Usuario não encontrado.' })
-            }
-
-        } catch (erro) {
-            res.status(500).json({
-                erro: "Erro ao processar a solicitação.",
-                detalhe: erro.message,
-            });
-        }
-
-    });
-
-router.route("/stats")
-    .get(async (req, res) => {
-        try {
-            const stats = await query(`
+router.route("/stats").get(async (req, res) => {
+    try {
+        const stats = await query(`
                 SELECT
                     COUNT(tbUsuario.idUsuario) AS totalUsuarios,
                     COUNT(CASE WHEN tbUsuario.tipoEntidade = 'empresa' THEN tbUsuario.idUsuario END) AS totalEmpresas,
@@ -50,21 +46,53 @@ router.route("/stats")
                 LEFT JOIN
                     tbPostagem ON tbUsuario.idUsuario = tbPostagem.idUsuario
             `);
-    
-            if (stats.length === 0) {
-                return res.status(404).json({
-                    erro: "Nenhum dado encontrado.",
-                });
-            }
-    
-            return res.json(stats[0]);
-        } catch (erro) {
-            console.error(erro);
-            res.status(500).json({
-                erro: "Erro ao processar a solicitação.",
-                detalhe: erro.message,
+
+        if (stats.length === 0) {
+            return res.status(404).json({
+                erro: "Nenhum dado encontrado.",
             });
         }
-    });    
+
+        return res.json(stats[0]);
+    } catch (erro) {
+        console.error(erro);
+        res.status(500).json({
+            erro: "Erro ao processar a solicitação.",
+            detalhe: erro.message,
+        });
+    }
+});
+
+router.route("/setoresempresas").get(async (req, res) => {
+    try {
+        const setores = await query(`
+                SELECT
+                    COUNT(tbEmpresa.idEmpresa) AS totalEmpresas,
+                    nomeAreaAtuacao
+                FROM
+                    tbEmpresa
+                JOIN
+                    tbAreaAtuacaoEmpresa
+                ON
+                    tbEmpresa.idAreaAtuacaoEmpresa = tbAreaAtuacaoEmpresa.idAreaAtuacao
+                GROUP BY
+                    nomeAreaAtuacao
+            `);
+
+        if (setores.length === 0) {
+            return res.status(404).json({
+                erro: "Nenhum dado encontrado.",
+            });
+        }
+
+        return res.json(setores); // Retorne o array completo
+    } catch (erro) {
+        console.error(erro);
+        res.status(500).json({
+            erro: "Erro ao processar a solicitação.",
+            detalhe: erro.message,
+        });
+    }
+});
 
 export default router;
